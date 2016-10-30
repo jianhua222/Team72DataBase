ed8f88425e00abeff601febc27a7367ea426365f
jianhua222 <1993222king@gmail.com>
2016-10-30 17:11:02 -0400

    SQL

--- src/sample/Main/Main.java
+++ src/sample/Main/Main.java
@@ -5,6 +5,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Main extends Application {
 
@@ -16,8 +19,24 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            con = DriverManager.getConnection("jdbc:mysql://academic-mysql.cc.gatech.edu/cs4400_Team_72",
+                    "cs4400_Team_72",
+                    "mmZwNaCR");
+            if(!con.isClosed())
+                System.out.println("Successfully connected to " +
+                        "MySQL server using TCP/IP...");
+        } catch(Exception e) {
+            System.err.println("Exception: " + e.getMessage());
+        } finally {
+            try {
+                if(con != null)
+                    con.close();
+            } catch(SQLException e) {}
+        }
         launch(args);
     }
 }
