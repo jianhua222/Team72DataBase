24b6478957c877878c3fe04b48cf4d1d691c7073
Ranchyliu <lrq1992@gmail.com>
2016-11-06 23:33:57 -0500

    LoginController update

--- src/Controllers/LoginController.java
+++ src/Controllers/LoginController.java
@@ -1,9 +1,13 @@
 package Controllers;
 
+import Models.UserManagement;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -13,9 +17,24 @@ import java.io.IOException;
 public class LoginController {
     @FXML
     private Button registerButton;
+    //public LoginController(){}
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private TextField passwordField;
+    @FXML
+    private void loginButtonpressed(){
+        UserManagement.login(this.usernameField.getText(), this.passwordField.getText());
+    }
+    @FXML
     private void registerButtonpressed(){
+        Parent root;
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("Register.fxml"));
+            root = FXMLLoader.load(getClass().getClassLoader().getResource("Main/Register.fxml"));
+            //Parent root = FXMLLoader.load(getClass().getResource("Main/Register.fxml"));
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
         }catch (IOException e){
 
         }
--- src/Models/UserManagement.java
+++ src/Models/UserManagement.java
@@ -9,6 +9,50 @@ import java.sql.*;
  */
 public class UserManagement {
     public User currentUser;
+    public static void login(String user_name, String password){
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            con = DriverManager.getConnection("jdbc:mysql://academic-mysql.cc.gatech.edu/cs4400_Team_72",
+                    "cs4400_Team_72",
+                    "mmZwNaCR");
+            if(!con.isClosed())
+                System.out.println("Successfully connected to " +
+                        "MySQL server using TCP/IP...");
+            //This will be the first sql statement!
+            ps = con.prepareStatement( "SELECT user_name, password FROM User WHERE user_name = ? AND password = ?" );
+            // 使用问号作为参数的标示
+
+            // 进行参数设置
+            // 与大部分Java API中下标的使用方法不同，字段的下标从1开始，1代表第一个问号
+            // 当然，还有其他很多针对不同类型的类似的PreparedStatement.setXXX()方法
+            ps.setString(1, user_name);
+            ps.setString(2, password );
+
+            // 结果集
+            rs = ps.executeQuery();
+
+            if ( rs.next() ) {
+                System.out.println("Successfully Login");
+            }else {
+                System.out.println("Login failed");
+            }
+
+        } catch(Exception e) {
+            System.err.println("Exception: " + e.getMessage());
+        } finally {
+            try {
+                if(con != null)
+                    rs.close();
+                    ps.close();
+                    con.close();
+            } catch(SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
     public static void register(String user_name, String GT_email,String password){
         Connection con = null;
         Statement stmt;
@@ -27,7 +71,7 @@ public class UserManagement {
             String sql;
             sql= "INSERT INTO User (user_name, password, UserType, GT_email) " +
                     "VALUES ('"+user_name+"', '"+password+"', "+"'Student', '"+GT_email+"' );";
-            System.out.println(sql);
+            //System.out.println(sql);
             stmt.executeUpdate(sql);
             System.out.println("Inserted records into the table...");
             //Clean-up environment
