3968d224d0f322c6709975cfed3265a57900c806
Ranchyliu <lrq1992@gmail.com>
2016-11-20 16:43:05 -0500

    mainpage controller created

---
+++ src/Controllers/MainPageController.java
@@ -0,0 +1,73 @@
+package Controllers;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Observable;
+
+/**
+ * Created by Ranchy on 11/20/2016.
+ */
+public class MainPageController {
+    @FXML
+    ListView<String> listView;
+
+    public void initialize(){
+        Connection con = null;
+        PreparedStatement ps1 = null;
+        PreparedStatement ps2 = null;
+        ResultSet rs = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            con = DriverManager.getConnection("jdbc:mysql://academic-mysql.cc.gatech.edu/cs4400_Team_72",
+                    "cs4400_Team_72",
+                    "mmZwNaCR");
+            if(!con.isClosed())
+                System.out.println("Successfully connected to " +
+                        "MySQL server using TCP/IP...");
+            //This will be the first sql statement!
+            ps1 = con.prepareStatement( "SELECT cname FROM Course" );
+            ps2 = con.prepareStatement( "SELECT pname FROM Project" );
+            // 使用问号作为参数的标示
+
+            //ps.setString(1, user_name);
+            //ps.setString(2, password );
+
+            // 结果集
+            rs = ps1.executeQuery();
+            ObservableList<String> tem = FXCollections.observableArrayList();
+            while (rs.next()) {
+
+                tem.add(rs.getString("cname"));
+
+            }
+
+            rs = ps2.executeQuery();
+            while (rs.next()) {
+
+                tem.add(rs.getString("pname"));
+
+            }
+            listView.setItems(tem);
+
+
+        } catch(Exception e) {
+            System.err.println("Exception: " + e.getMessage());
+
+        } finally {
+            try {
+                if(con != null)
+                    rs.close();
+                ps1.close();
+                ps2.close();
+                con.close();
+            } catch(SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
