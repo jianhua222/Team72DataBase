7a9e5da10d5cff8b9940b89cd487ed1a9b598789
qzheng61 <qzheng61@gatech.edu>
2016-11-22 14:06:08 -0500

    Project model created

--- src/Controllers/MeController.java
+++ src/Controllers/MeController.java
@@ -10,22 +10,25 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 /**
- * Created by Allen on 11/22/2016.
+ * Created by Qi on 11/22/2016.
  */
 public class MeController {
+
+
     @FXML
     private Button backButton;
     @FXML
-    private void backButtonPressed(){
+    private void backButtonpressed(){
         try{
             Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Main/MainPage.fxml"));
             Stage primaryStage = (Stage) backButton.getScene().getWindow();
-            primaryStage.setTitle("MainPage");
+            primaryStage.setTitle("Main");
             primaryStage.setScene(new Scene(root, 600, 400));
             primaryStage.show();
         }
         catch (IOException e){
 
         }
+
     }
 }
